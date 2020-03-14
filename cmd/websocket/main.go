44d8787832c4ea0acc0011d0219faec259defcc9
Albert <albertjuhe@gmail.com>
2020-03-14 13:47:46 +0100

    websocket server

--- cmd/websocket/main.go
+++ cmd/websocket/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main(){
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
 
+var upgrades = websocket.Upgrader{}
+
+func reverse(w http.ResponseWriter, r *http.Request) {
+	ws, _ := upgrades.Upgrade(w, r, nil)
+	defer ws.Close()
+
+	for {
+		_, message,_ := ws.ReadMessage()
+		log.Printf("Message received: %s", message)
+	}
+}
+
+func main() {
+	http.HandleFunc("/reverse", reverse)
+	log.Fatal(http.ListenAndServe(":5555", nil))
 }
