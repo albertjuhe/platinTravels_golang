fc5ad54f0a6af80485b03a395f5d1a54bf46bba7
Albert <albertjuhe@gmail.com>
2020-03-15 18:14:23 +0100

    create web socket connection

--- cmd/websocket/main.go
+++ cmd/websocket/main.go
@@ -7,20 +7,38 @@ import (
 	"net/http"
 )
 
-var upgrades = websocket.Upgrader{}
+var upgrades = websocket.Upgrader{
+	ReadBufferSize:1024,
+	WriteBufferSize:1024,
+}
 
-func reverse(w http.ResponseWriter, r *http.Request) {
-	ws, _ := upgrades.Upgrade(w, r, nil)
-	defer ws.Close()
-
+func reader(conn *websocket.Conn) {
 	for {
-		_, message,_ := ws.ReadMessage()
+		_, message, _ := conn.ReadMessage()
 		log.Printf("Message received: %s", message)
 	}
 }
 
+func ping(w http.ResponseWriter, r *http.Request) {
+	upgrades.CheckOrigin = func(r *http.Request) bool {return true}
+
+	ws, _ := upgrades.Upgrade(w, r, nil)
+	defer ws.Close()
+
+	reader(ws)
+}
+
+func alive(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "server is Alive")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/ping", ping)
+	http.HandleFunc("/", alive)
+}
+
 func main() {
-	fmt.Println("Starttin websocket server al :5555")
-	http.HandleFunc("/reverse", reverse)
+	fmt.Println("Starting websocket server al :5555")
+	setupRoutes()
 	log.Fatal(http.ListenAndServe(":5555", nil))
 }
